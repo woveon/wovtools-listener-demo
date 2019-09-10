6ed282841e8ab20da3d374586ecdd13e91a01f5a
Chad Wingrave <chadwick@woveon.com>
2019-09-10 13:49:25 -0400

    Changes.

--- bin/cleanbetweendemos.sh
+++ bin/cleanbetweendemos.sh
@@ -4,8 +4,11 @@
 DIR=~/code/wtdl
 ME=cw
 
+echo "...deleting any files created during a demo run"
 rm -f ~/.wovtools
 rm -Rf ~/.wovtools_secrets
+rm -Rf node_modules
+rm -f yarn.lock
 rm -Rf ${DIR}/wovtools
 rm -f ${DIR}/src/wtdlconfig.sh
 
@@ -23,13 +26,17 @@ mkdir ${DIR}_dba.git
 git -C ${DIR}_dba.git init --bare
 
 
-# change this remote to local repo
+echo "...change this remote to local repo"
 git remote set-url origin ${DIR}_test.git
 
 # Change it back with
 # git remote set-url origin git@github.com:woveon/wtdl.git
 
+echo "...delete any Kubernetes namespaces or context created during a demo"
 kubectl delete namespace wtd-${ME}   2> /dev/null
 kubectl delete namespace wtd-dev   2> /dev/null
 kubectl delete context wov-aws-va-grape-wtd-${ME}   2> /dev/null
 kubectl delete context wov-aws-va-grape-wtd-dev   2> /dev/null
+
+echo "...delete any AWS ECR container repos"
+aws ecr delete-repository --repository-name wtd/wtdl
